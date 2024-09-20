92350f66c4d0d9b001de2f973fbed899b9b880ff
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-09-19 21:45:36 -0400

    refactor setting up channels is one command

--- src/commands/setup/setModCreation.js
+++ src/commands/setup/setModCreation.js
@@ -6,19 +6,32 @@ const dataFileName = '../../data.json';
 const data = require(dataFileName);
 const apiCalls = require('../../utils/apiCalls');
 
+function removeDuplicates(arr) {
+    return arr.filter(function (item, pos) { return arr.indexOf(item) == pos; })
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('set-mod-creation')
-        .setDescription('Sets the #mod-creation channel')
+        .setName('set-channels')
+        .setDescription('(admin only) Sets the relevant channels')
         .addChannelOption(option =>
-            option.setName('channel')
+            option.setName('mod-creation')
                 .setDescription('The channel that will be set as mod-creation')
-                .setRequired(true)),
+                .setRequired(true))
+        .addChannelOption(option =>
+            option.setName('repo-requests')
+                .setDescription('The channel that will be set as repo-requests')
+                .setRequired(true))
+        .addChannelOption(option =>
+            option.setName('repo-discussion')
+                .setDescription('The channel that will be set as repo-discussion')
+                .setRequired(true))
+    ,
 
     async execute(client, interaction) {
         await interaction.deferReply({
             ephemeral: true
-          });
+        });
 
         //make it so only admins can run this command
         const user = await apiCalls.getServerMember(interaction.user.id);
@@ -26,21 +39,34 @@ module.exports = {
 
             interaction.editReply({
                 content: "Only admins can use this command",
-              });
+            });
             return;
         }
 
-        //set the mod-creation channel
-        data.modCreationId = interaction.options.get('channel').value;
+        //set the channels
+        const channels = [
+            data.modCreationId = interaction.options.get('mod-creation').value,
+            data.repoRequests = interaction.options.get('repo-requests').value,
+            data.repoDiscussion = interaction.options.get('repo-discussion').value,
+        ]
+
+
+        //verify none of the channels are the same
+        if (removeDuplicates(channels).length != 3) {
+            interaction.editReply({ content: `Can't have channels be the same` });
+            return;
+        }
 
         fs.writeFile(path.join(__dirname, dataFileName), JSON.stringify(data, null, 2), (err) => {
             if (err) return console.log(err);
-            console.log(JSON.stringify(data, null, 2));
-            console.log('writing to ' + dataFileName);
         });
-        
+
+
         interaction.editReply({
-            content: `Successfully set <#${data.modCreationId}> as mod-creation`,
+            content: `Successfully set:
+- mod-creation: <#${data.modCreationId}>
+- repo-requests: <#${data.repoRequests}> 
+- repo-discussion: <#${data.repoDiscussion}>`,
         });
 
     }
