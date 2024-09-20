07ba7800c5c25baca554b66721295a1e963112d0
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-09-19 21:01:27 -0400

    Finish set-mod-creation

--- src/commands/setup/setModCreation.js
+++ src/commands/setup/setModCreation.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
+const path = require('path');
 const { SlashCommandBuilder } = require('discord.js')
 const fs = require('fs');
-const dataFileName = '../../../data.json';
+const dataFileName = '../../data.json';
 const data = require(dataFileName);
 const apiCalls = require('../../utils/apiCalls');
 
@@ -15,29 +16,33 @@ module.exports = {
                 .setRequired(true)),
 
     async execute(client, interaction) {
-        await interaction.deferReply();
+        await interaction.deferReply({
+            ephemeral: true
+          });
 
         //make it so only admins can run this command
         const user = await apiCalls.getServerMember(interaction.user.id);
         if (!user.roles.some(roleId => process.env.ADMIN_ROLE_IDS.includes(roleId))) {
-            //todo: make it so only the user can see this reply
-            interaction.editReply("Only admins can use this command");
+
+            interaction.editReply({
+                content: "Only admins can use this command",
+              });
             return;
         }
 
-        console.log(data);
-
+        //set the mod-creation channel
         data.modCreationId = interaction.options.get('channel').value;
 
-        await fs.writeFile(dataFileName, JSON.stringify(data, null, 2), function writeJSON(err) {
+        fs.writeFile(path.join(__dirname, dataFileName), JSON.stringify(data, null, 2), (err) => {
             if (err) return console.log(err);
             console.log(JSON.stringify(data, null, 2));
             console.log('writing to ' + dataFileName);
         });
+        
+        interaction.editReply({
+            content: `Successfully set <#${data.modCreationId}> as mod-creation`,
+        });
 
-        interaction.editReply("Successfully set {insert channel here} as mod-creation");
-
-        //todo set the mod-creation channel
     }
 
 }
