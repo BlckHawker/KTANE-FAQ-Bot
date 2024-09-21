3c3e5fe74c7def8af0173b252a26e836f5ea1c36
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-09-20 23:00:09 -0400

    Make help message dynamic to channels

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -1,5 +1,6 @@
 const { Client, Message, EmbedBuilder, SlashCommandBuilder } = require('discord.js')
 const pagination = require('../../utils/pagination');
+const utils = require('../../utils/utils');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,8 +9,9 @@ module.exports = {
 
     async execute(client, interaction) {
         
-        
-        const introMessage = `This bot’s purpose is to answer frequently asked questions about modding KTANE. Its goal is to alleviate answering duplicate questions in #mod-creation and #repo-discussion. Please try to see if your question(s) are answered here before asking questions there. Please report any bugs about the bot via a github issue [insert link here].`;
+        const { serverObjIndex, server } = utils.getDataServerObject(interaction.guild.id);
+
+        const introMessage = `This bot's purpose is to answer frequently asked questions about modding KTANE. Its goal is to alleviate answering duplicate questions in <#${server.modCreationId}> and <#${server.repoDiscussionId}>. Please try to see if your question(s) are answered here before asking questions there. Please report any bugs about the bot via a [github issue](https://github.com/BlckHawker/KTANE-FAQ-Bot/issues).`;
         const embeds = [];
         const pageDescription = [];
         for (let i = 0; i < 4; i++) {
