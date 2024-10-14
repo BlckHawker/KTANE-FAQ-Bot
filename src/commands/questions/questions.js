b7ba9ae63fa57b0b8cbaf86829c53b4c3328655e
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-10-14 15:08:36 -0400

    make subcommands dynamic

--- src/commands/questions/questions.js
+++ src/commands/questions/questions.js
@@ -1,83 +1,31 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { sendQuestion } = require('../../utils/utils.js');
 const questionJSON = require('../../questions.json');
+
+function makeSlashCommand() {
+	const slashCommand = new SlashCommandBuilder()
+	.setName('questions')
+	.setDescription('Test command that provides information about the user.')
+
+	const max = 100;
+	for(const questionObj of questionJSON["questions"]) {
+		let description = questionObj.question.substring(0, max - 4);
+		if(description.length !== questionObj.question.length) {
+			description += '...'
+		}
+		slashCommand
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName(`${questionObj.commandName}`)
+				.setDescription(description))
+	}
+
+	return slashCommand;
+}
+
 //todo dynamically make these slash commands
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('questions')
-		.setDescription('Test command that provides information about the user.')
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('help')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('question-not-here')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('modding-start')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('c-sharp-tutorials')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('mod-tutorials')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('shortcut')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('github')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('mod-reference')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('create-manual')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('add-manual')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('change-manual')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('request-delay')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('become-maintainer')
-				.setDescription('This does not matter'))				
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('mod-not-working')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('text-see-through')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('mod-building-error')
-				.setDescription("This does not matter"))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('no-sound')
-				.setDescription('This does not matter'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('on-interact-delegate')
-				.setDescription('This does not matter')),
+	data: makeSlashCommand(),
 
 	async execute(client, interaction) {
 		const commandName = interaction.options.getSubcommand();
